36f82c66df1fe4aa3b87ac50ea50671b0ee34f58
pinktopaz <heather128@ewhain.net>
2024-04-14 11:33:50 +0900

    fix : pwa 404 에러 해결

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,6 +1,6 @@
 export function registerServiceWorker() {
   navigator.serviceWorker
-    .register("firebase-messaging-sw.js")
+    .register("../../public/firebase-messaging-sw.js")
     .then(function (registration) {
       console.log("Service Worker 등록 성공:", registration);
     })
